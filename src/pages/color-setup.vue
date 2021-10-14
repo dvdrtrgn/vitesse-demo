c0ca908a9b8332b738cb2cdbc3aa5612facc6ee0
dvdrtrgn <dvdrtrgn@gmail.com>
2021-10-14 10:51:51 -0500

    Demo setup and css bind

---
+++ src/pages/color-setup.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+const myColor = ref('red')
+</script>
+
+<template>
+  <button @click="myColor = myColor === 'red' ? 'green' : 'red'">
+    ▶️[ My color is {{ myColor }} ]◀️
+  </button>
+</template>
+
+<style>
+  button {
+    color: v-bind(myColor);
+  }
+</style>
